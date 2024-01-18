0af9c67e947461129daf2e8bf9e1026a4d68f1c7
hussainahmed1234 <156226006+hussainahmed1234@users.noreply.github.com>
2024-01-18 22:02:52 +0500

    Add files via upload

---
+++ hussain_midterm.py
@@ -0,0 +1,30 @@
+#hussain midtre mexam 
+#Q1 - Write a Python program to do arithmetical operations addition and division.
+#addition
+num1=5
+num2=6
+sum=num1+num2
+print(sum)
+
+#division
+num1=int(input("first_number"))
+num2=int(input("second_number"))
+division=num1/num2
+print(division)
+
+#Q2 - Write a Python program to find the area of a triangle
+length=int(input("base of triangle"))
+hight=int(input("hight of triangle"))
+area=1/2*length*hight
+print(area)
+
+#Q3 - Write a Python program to convert Celsius to Fahrenheit.
+celsius=int(input("celsius"))
+Fahrenheit=(9/5*celsius)+32
+print(Fahrenheit)
+
+#Q4 - Write a Python program that return all datatypes that we discussed in the class.
+data type of 19 is integer
+data type of is 16.5 float
+data type of  hussain str
+data type of True is bool
